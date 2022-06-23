4a31a7f2db4d84c67bca5121d56031e34b254ca7
MaidenlessEldenLord <greggjamal@gmail.com>
2022-06-23 18:44:47 -0400

    Settig up Redux

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,4 +1,5 @@
-import { ScrollView } from "react-native-gesture-handler"
+import { Card } from "react-native-elements";
+import { ScrollView, Text } from "react-native";
 
 
 
@@ -6,7 +7,15 @@ const ContactScreen = () => {
 
     return ( 
         <ScrollView>
-
+            <Card wrapperStyle={{margin: 10}}> 
+                <Card.Title>Contact Information</Card.Title>
+                <Card.Divider /> 
+                <Text>1 Nucamp Way</Text>
+                <Text>Seattle, WA 98001 </Text> 
+                <Text style={{ marginBottom: 10}}>U.S.A</Text>
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text>Email: campsites@nucamp.co</Text>            
+                </Card>
         </ScrollView>
     );
 }; 
---
+++ shared/baseURL.js
@@ -0,0 +1,2 @@
+export const baseUrl = 'http://192.168.0.43:3001/';
+
